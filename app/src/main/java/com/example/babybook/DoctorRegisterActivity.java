175130a8c100a1816152aa8f8b26f7137e16abd9
angelocagas <yourname@email.address>
2024-09-15 15:41:07 +0800

    parent and doctor can upload profile pics

--- app/src/main/java/com/example/babybook/DoctorRegisterActivity.java
+++ app/src/main/java/com/example/babybook/DoctorRegisterActivity.java
@@ -1,7 +1,10 @@
 package com.example.babybook;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.Patterns;
@@ -15,14 +18,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,6 +42,10 @@ public class DoctorRegisterActivity extends AppCompatActivity {
     private Spinner spinnerSpecialization;
     private FirebaseAuth mAuth;
     private FirebaseFirestore db;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private boolean isImageSelected = false;
+    private ImageView backBtn, selectedImage;
+    private Uri selectedImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +63,23 @@ public class DoctorRegisterActivity extends AppCompatActivity {
         editTextSpecialization = findViewById(R.id.editTextSpecialization);
         editTextClinicAddress = findViewById(R.id.editTextClinicAddress);
         spinnerSpecialization = findViewById(R.id.spinnerspecial);
+
+        selectedImage = findViewById(R.id.ivSelectedImage);
+        backBtn = findViewById(R.id.imageView);
+
+        ConstraintLayout btnUploadProfile = findViewById(R.id.btnAddImage);
+
+        //SELECT PROFILE PICTURE
+
+        btnUploadProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Profile Photo to Continue"), PICK_IMAGE_REQUEST);
+            }
+        });
 
         ArrayAdapter<CharSequence> specializationAdapter = ArrayAdapter.createFromResource(
                 this,
@@ -93,7 +122,6 @@ public class DoctorRegisterActivity extends AppCompatActivity {
     }
 
 
-
     private void registerDoctor() {
         //final String fullName = editTextFullName.getText().toString().trim();
         String firstName = editTextFirstName.getText().toString().trim();
@@ -105,6 +133,7 @@ public class DoctorRegisterActivity extends AppCompatActivity {
         String clinicAddress = editTextClinicAddress.getText().toString().trim();
         int selectedSpecializationPosition = spinnerSpecialization.getSelectedItemPosition();
         String specialization = spinnerSpecialization.getSelectedItem().toString().trim();
+        TextView selectProfile = findViewById(R.id.textView4);
 
 
         if (TextUtils.isEmpty(firstName)) {
@@ -150,7 +179,7 @@ public class DoctorRegisterActivity extends AppCompatActivity {
             return;
         }
 
-        if (selectedSpecializationPosition == 0 ) {
+        if (selectedSpecializationPosition == 0) {
             Toast.makeText(this, "Please select specialization", Toast.LENGTH_SHORT).show();
             editTextSpecialization.requestFocus();
             spinnerSpecialization.requestFocus();
@@ -163,6 +192,13 @@ public class DoctorRegisterActivity extends AppCompatActivity {
             return;
         }
 
+        if (!isImageSelected) {
+            selectProfile.setError("Select a profile photo");
+            Toast.makeText(this, "Select a profile photo", Toast.LENGTH_SHORT).show();
+            selectProfile.requestFocus();
+            return;
+        }
+
         // Register doctor with Firebase Authentication
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -171,11 +207,14 @@ public class DoctorRegisterActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign up success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
+
+                            // Get the user ID
+                            String userId = mAuth.getCurrentUser().getUid();
+
+                            // Call the uploadProfilePicture method
+                            uploadProfilePicture(userId, fullName, email, specialization, clinicAddress);
+
                             Toast.makeText(DoctorRegisterActivity.this, "Doctor Registration successful", Toast.LENGTH_SHORT).show();
-
-                            // Save additional doctor data to Firestore
-                            saveDoctorData(fullName, email, specialization, clinicAddress);
-
                             // Redirect to doctor dashboard
                             redirectToLogin();
                         } else {
@@ -188,7 +227,7 @@ public class DoctorRegisterActivity extends AppCompatActivity {
                 });
     }
 
-    private void saveDoctorData(String fullName, String email, String specialization, String clinicAddress) {
+    private void saveDoctorData(String fullName, String email, String specialization, String clinicAddress, String profileImageUrl) {
         // Ensure user is authenticated
         if (mAuth.getCurrentUser() == null) {
             Log.w(TAG, "saveDoctorData: no authenticated user");
@@ -203,6 +242,8 @@ public class DoctorRegisterActivity extends AppCompatActivity {
         doctor.put("email", email);
         doctor.put("specialization", specialization);
         doctor.put("clinicAddress", clinicAddress);
+        doctor.put("profileImageUrl", profileImageUrl); // Store the profile image URL
+
 
         // Add a new document with a generated ID
         db.collection("doctorUsers")
@@ -235,5 +276,52 @@ public class DoctorRegisterActivity extends AppCompatActivity {
         finish(); // Finish the current activity to prevent going back to it via back button
     }
 
+    //TO DISPLAY THE SELECTED IMAGE TO IMAGEVIEW
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            selectedImageUri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImageUri);
+                // Display the selected image or do any other processing as needed
+                // For example, you can set it in an ImageView:
+                selectedImage.setImageBitmap(bitmap);
+
+                isImageSelected = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // uploadProfilePicture to accept fullName and email
+    private void uploadProfilePicture(String userId, String fullName, String email, String specialization, String clinicAddress) {
+        if (selectedImageUri != null) {
+            StorageReference storageRef = FirebaseStorage.getInstance().getReference().child("doctor_profile_pictures");
+            StorageReference imageRef = storageRef.child(userId + ".jpg");
+
+            imageRef.putFile(selectedImageUri)
+                    .addOnSuccessListener(taskSnapshot -> {
+                        // Image uploaded successfully
+                        imageRef.getDownloadUrl().addOnCompleteListener(downloadUrlTask -> {
+                            if (downloadUrlTask.isSuccessful()) {
+                                String imageUrl = downloadUrlTask.getResult().toString();
+
+                                // After successfully uploading the image, save user data with profile image URL
+                                saveDoctorData(fullName, email, specialization, clinicAddress, imageUrl);
+                            } else {
+                                // Handle error while getting the image URL
+                            }
+                        });
+                    })
+                    .addOnFailureListener(e -> {
+                        // Handle image upload failure
+                    });
+        }
+    }
 }
+
+
