a1a52d0845e2f2102f3c4434aed375f6f0b3b0b8
babbb06 <joshua.balaoro4@gmail.com>
2024-10-20 13:19:40 +0800

    splash done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,7 +74,7 @@ dependencies {
     implementation ("com.hbb20:ccp:2.7.3")
     implementation ("com.google.android.material:material:1.9.0")
     implementation ("com.google.android.gms:play-services-maps:18.1.0")
-
+    implementation ("androidx.viewpager2:viewpager2:1.0.0")
     implementation ("com.google.android.flexbox:flexbox:3.0.0")
     implementation ("com.github.bumptech.glide:glide:4.15.1")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
